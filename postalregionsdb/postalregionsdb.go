ccd1fbec64c4d8c2d2777dca67003a97a1917fe3
Tom Taylor <tom@tomtaylor.co.uk>
2024-03-02 15:44:24 +0000

    Set postcodes into postalregion hierarchy

---
+++ postalregionsdb/postalregionsdb.go
@@ -0,0 +1,78 @@
+package postalregionsdb
+
+import (
+	"os"
+	"strings"
+
+	"github.com/saracen/walker"
+	"github.com/whosonfirst/go-whosonfirst-feature/properties"
+)
+
+type PostalRegion struct {
+	Name      string
+	WofID     int64
+	Hierarchy []map[string]int64
+}
+
+type PostalRegionsDB struct {
+	dataPath *string
+	Regions  map[string]*PostalRegion
+}
+
+func NewPostalRegionsDB(dataPath string) *PostalRegionsDB {
+	db := &PostalRegionsDB{dataPath: &dataPath}
+
+	return db
+}
+
+func (db *PostalRegionsDB) Build() error {
+	walkFn := func(path string, fi os.FileInfo) error {
+		if fi.IsDir() {
+			return nil
+		}
+
+		if !strings.HasSuffix(path, ".geojson") {
+			return nil
+		}
+
+		f, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		placetype, err := properties.Placetype(f)
+		if err != nil {
+			return err
+		}
+
+		if placetype != "postalregion" {
+			return nil
+		}
+
+		name, err := properties.Name(f)
+		if err != nil {
+			return err
+		}
+
+		id, err := properties.Id(f)
+		if err != nil {
+			return err
+		}
+
+		hierarchy := properties.Hierarchies(f)
+
+		db.Regions[name] = &PostalRegion{
+			Name:      name,
+			WofID:     id,
+			Hierarchy: hierarchy,
+		}
+
+		return nil
+	}
+
+	errorFn := walker.WithErrorCallback(func(path string, err error) error {
+		return err
+	})
+
+	return walker.Walk(*db.dataPath, walkFn, errorFn)
+}
